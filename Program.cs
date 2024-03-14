45f4cf2d7e1ff8d12e2d68392e0d857b4b794e71
IrjiSyahrul <irjisyahrulchannel@gmail.com>
2024-03-14 10:12:09 +0700

    menambahkan class KodeBuah

--- Program.cs
+++ Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        class Kodebuah
+        {
+            public enum getBuah
+            {
+                Apel,Aprikot,Alpukat,Pisang,Paprika,Blackberry,Ceri,Kelapa,Jagung
+            }
+
+            public static string getKodeBuah(getBuah inputan)
+            {
+                string[] kodebuah = {"A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+                return kodebuah[(int)inputan];
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Kodebuah.getKodeBuah(Kodebuah.getBuah.Apel));
         }
     }
 }
--- obj/Debug/net8.0/Modul4_1302220088.AssemblyInfo.cs
+++ obj/Debug/net8.0/Modul4_1302220088.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul4_1302220088")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d11e0f56e4840adf807c64a5878dd729f6a107c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul4_1302220088")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul4_1302220088")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
