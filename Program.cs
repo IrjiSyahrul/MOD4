4d11e0f56e4840adf807c64a5878dd729f6a107c
IrjiSyahrul <irjisyahrulchannel@gmail.com>
2024-03-14 09:52:52 +0700

    Inisialisasi Proyek

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul4_1302220088
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
